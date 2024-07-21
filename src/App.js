557d710400f6714d06e7d0923eeebb7261ab339e
Sabau Daniel Catalin <111538225+Sabau-Daniel@users.noreply.github.com>
2024-07-21 18:27:36 +0300

    changed location and added some search features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card, CardBody} from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+
+const CLIENT_ID = "4eb0d6ccfaee4bbf99900fa7078a1b3e";
+const CLIENT_SECRET = "564fbbd357b34fce9b04e83ea7161bb8";
+
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+
+  useEffect(() => {
+    // API aceess token
+    var authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
+  }, [])
+
+  //Search
+  async function search(){
+    console.log("Search for " + searchInput ); //Taylor Swift
+
+    // Get request using search to get the Artist ID
+    var searchParameters = {
+      methodL: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+
+      }
+    }
+    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist' ,searchParameters)
+    .then(response => response.json())
+    .then(data => {return data.artists.items[0].id})
+
+    console.log("artist ID is " + artistID);
+    // Get request with Artist ID grab all the albums from that artist
+    var albums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
+    //Display those albums to the user
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Container>
+      <InputGroup className='mb-3' size='lg'>
+      <FormControl
+        placeholder='Seach For Artist'
+        type='input'
+        onKeyDown={event =>{
+          if (event.key == "Enter"){
+            search();
+          }
+        }}
+        onChange={event => setSearchInput(event.target.value)}
+      />
+      <Button onClick={search}>
+        Search
+      </Button>
+      </InputGroup>
+     </Container>
+     <Container>
+      <Row className='mx-2 row row-cols-4'>
+      <Card>
+          <Card.Img src ='#' />
+          <Card.Body>
+            <Card.Title>
+              Album Name
+            </Card.Title>
+          </Card.Body>
+        </Card>
+      </Row>
+      
+     </Container>
     </div>
   );
 }
