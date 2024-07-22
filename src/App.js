b0c4e05b2e4a57d819d427182ba9424d88e6159c
Sabau Daniel Catalin <111538225+Sabau-Daniel@users.noreply.github.com>
2024-07-22 18:00:04 +0300

    added additional comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Row, Card, CardBody} from 'react-bootstrap';
+import { Container, InputGroup, FormControl, Button, Row, Card, CardBody } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 
+// Spotify API credentials
 const CLIENT_ID = "4eb0d6ccfaee4bbf99900fa7078a1b3e";
 const CLIENT_SECRET = "564fbbd357b34fce9b04e83ea7161bb8";
 
-
 function App() {
+  // State variables
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
-    // API aceess token
+    // Fetch access token from Spotify API
     var authParameters = {
       method: 'POST',
       headers: {
-        'Content-type' : 'application/x-www-form-urlencoded'
+        'Content-type': 'application/x-www-form-urlencoded'
       },
       body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
     }
@@ -26,72 +27,74 @@ function App() {
       .then(data => setAccessToken(data.access_token))
   }, [])
 
-  //Search
-  async function search(){
-    console.log("Search for " + searchInput ); //Taylor Swift
+  // Search function to find artist and their albums
+  async function search() {
+    console.log("Search for " + searchInput); // Log the search input
 
     // Get request using search to get the Artist ID
     var searchParameters = {
-      methodL: 'GET',
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + accessToken
-
       }
     }
-    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist' ,searchParameters)
-    .then(response => response.json())
-    .then(data => {return data.artists.items[0].id})
+    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
+      .then(response => response.json())
+      .then(data => { return data.artists.items[0].id })
 
-    console.log("artist ID is " + artistID);
-    // Get request with Artist ID grab all the albums from that artist
+    console.log("Artist ID is " + artistID); // Log the artist ID
+
+    // Get request with Artist ID to grab all the albums from that artist
     var albums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setAlbums(data.items);
-    });
-    //Display those albums to the user
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setAlbums(data.items);
+      });
+    // Display those albums to the user
   }
-console.log(albums);
+
+  console.log(albums); // Log the albums array
+
   return (
     <div className="App">
-     <Container>
-      <InputGroup className='mb-3' size='lg'>
-      <FormControl
-        placeholder='Seach For Artist'
-        type='input'
-        onKeyDown={event =>{
-          if (event.key == "Enter"){
-            search();
-          }
-        }}
-        onChange={event => setSearchInput(event.target.value)}
-      />
-      <Button onClick={search}>
-        Search
-      </Button>
-      </InputGroup>
-     </Container>
-     <Container>
-      <Row className='mx-2 row row-cols-4'>
-        {albums.map( (album, i) => {
-          console.log(album);
-          return(
-            <Card>
-              <Card.Img src = {album.images[0].url} />
+      <Container>
+        {/* Input group for searching artist */}
+        <InputGroup className='mb-3' size='lg'>
+          <FormControl
+            placeholder='Search For Artist'
+            type='input'
+            onKeyDown={event => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={event => setSearchInput(event.target.value)}
+          />
+          <Button onClick={search}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        {/* Displaying albums in a responsive grid */}
+        <Row className='mx-2 row row-cols-4'>
+          {albums.map((album, i) => {
+            console.log(album); // Log each album
+            return (
+              <Card key={i}>
+                <Card.Img src={album.images[0].url} />
                 <Card.Body>
                   <Card.Title>
-                   {album.name}
+                    {album.name}
                   </Card.Title>
                 </Card.Body>
-            </Card>
-          )
-        })}
-      
-      </Row>
-      
-     </Container>
+              </Card>
+            )
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
